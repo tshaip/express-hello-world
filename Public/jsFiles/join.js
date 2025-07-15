9173366f49a627b78b8513ab58569c415398de62
tshaip <jonasjuhrich@gmail.com>
2025-07-15 22:03:51 +0200

    v7

--- Public/jsFiles/join.js
+++ Public/jsFiles/join.js
@@ -9,36 +9,39 @@ function joinRoom () {
             const lbl = document.getElementById('FehlerRaumName');
             lbl.textContent = `Room with the name '${roomName}' does not exist. Please check the code.`;
             console.log(`Error: Room with name '${roomName}' not found.`);
-            return;
+            return Promise.reject(`Room with name '${roomName}' not found.`);
         }
         return response.json();
     })
     .then(jsonData => {
+      if (!jsonData) return;
+
       if (jsonData.players.includes(username)) {
         const lbl = document.getElementById('FehlerSpielerName');
         lbl.textContent = `Username is not available. Please choose a different username.`;
-        return;
+        return Promise.reject(`Username '${username}' is already taken.`);
       }
 
       console.log("Room data retrieved:", jsonData);
 
       localStorage.setItem("playerName", username);
+      localStorage.setItem("categories", JSON.stringify(jsonData.categories));
+
       jsonData.players.push(username);
-      fetch('/rooms/room-data/' + roomName, {
+
+      return fetch('/rooms/room-data/' + roomName, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(jsonData)
-      }) 
-      .then(res => res.text())
-      .then(msg => {
-        console.log('Serverantwort:', msg);})
-      .catch(err => console.error('Fehler:', err));
-
-      localStorage.setItem("categories", JSON.stringify(jsonData.categories));
+      });
+    }) 
+    .then(res => res.text())
+    .then(msg => {
+      console.log('Serverantwort:', msg);})
+    .then(() => {
+      window.location.href = "./pictureSubmit.html";
     })
     .catch(error => {
     console.error('Fehler beim Abrufen des Raums:', error.message);
     });
-
-    window.location.href = "./pictureSubmit.html";
 }
