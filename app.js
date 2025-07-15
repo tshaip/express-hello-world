7fd0d3cb414622c81bd45515c8449b53db43051c
tshaip <jonasjuhrich@gmail.com>
2025-07-15 22:45:56 +0200

    v2

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
@@ -8,6 +7,16 @@ app.use(express.static("Public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + '/Public/homepage.html'));
 
+app.use(express.json());
+
+//Routen importieren
+
+const roomRoutes = require('./routes/rooms');
+
+//Routen einbinden
+
+app.use('/', roomRoutes);
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
@@ -15,22 +24,3 @@ server.headersTimeout = 120 * 1000;
 
 // JSON-Body verarbeiten
 app.use(express.json());
-
-// POST: Raumdaten empfangen und speichern
-app.post('/Create-Room', (req, res) => {
-  const roomData = req.body;
-  const fileName = `${roomData.name || 'raum'}.json`;
-  const dirPath = path.join(__dirname, `data/${roomData.name}`);
-  const filePath = path.join(dirPath, fileName);
-
-  fs.mkdirSync(dirPath, { recursive: true });
-
-  fs.writeFileSync(filePath, JSON.stringify(roomData, null, 2), err => {
-    if (err) {
-      console.error('Fehler beim Speichern:', err);
-      return res.status(500).send('Fehler beim Speichern');
-    }
-    res.send('Raum erfolgreich gespeichert!');
-  });
-});
-
