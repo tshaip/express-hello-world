a16287841aaa44c408c34fb90c539257f7a4346f
tshaip <jonasjuhrich@gmail.com>
2025-07-15 16:53:08 +0200

    hurray

--- Public/jsFiles/create.js
+++ Public/jsFiles/create.js
@@ -1,47 +1,27 @@
-const categoryCount = 0;
+let categoryCount = 0;
 
 function addCategory (){
     const container = document.getElementById("categorys");
     const div = document.createElement("div");
 
     div.className = "category";
-    div.innerHTML = `<label>Category ' + categoryCount + ':</label>
+    div.innerHTML = `<label>Category ${categoryCount}:</label>
     <input type="text" name="category" placeholder="" required>`;
+
+    console.log("Category added:", div.innerHTML);
 
     container.appendChild(div);
     categoryCount++;
 }
 
-function tryName () {
-
-    const inputs = document.querySelectorAll('input[name="category"]');
-    const categorys = [];
-
-    inputs.forEach(input => {
-      const wert = input.value.trim();
-      if (wert !== '') {
-        categorys.push(wert);
+function createRoom() {
+    const roomName = document.getElementById("roomName").value;
+    fetch('/room-data/' + roomName,)
+    .then(response => {
+      if (!response.ok) {
+        const lbl = document.getElementById('FehlerRaumName');
+        lbl.textContent = `Room with the name '${roomName}' already exists. Please choose a different name.`;
+        return;
       }
-    });
-
-    const Room = {
-        name : document.getElementById("roomName").value,
-        password_value : document.getElementsById("Privat").checked,
-        password : document.getElementById("password").value,
-        categorys : categorys,
-        players : {},
-    }
- 
-    const jsonData = JSON.stringify(Room, null, 2);
-    console.log("Daten als JSON:", jsonData);
-
-    // An den Server senden
-    fetch('/Create-room', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: jsonData
     })
-    .then(res => res.text())
-    .then(msg => alert('Antwort vom Server: ' + msg))
-    .catch(err => console.error('Fehler:', err));
 }
